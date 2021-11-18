3f938caad3f94f703d6ab0c5ff64d03416d04753
Laminariy <dovakin121212@gmail.com>
2021-11-18 16:58:30 +0300

    keyboard

--- vk-luvit/init.lua
+++ vk-luvit/init.lua
@@ -1,9 +1,14 @@
 local API = require("./api")
 local LongPoll = require("./longpoll")
 local Blueprint = require("./blueprint")
+local Keyboard = require("./keyboard")
 return {
   API = API,
   LongPoll = LongPoll,
   Bot = LongPoll,
-  Blueprint = Blueprint
+  Blueprint = Blueprint,
+  Keyboard = Keyboard,
+  kb = Keyboard.Keyboard,
+  kb_colors = Keyboard.COLORS,
+  kb_actions = Keyboard.ACTIONS
 }
