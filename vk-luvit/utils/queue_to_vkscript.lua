eb24f2c1d070fe080e4958e834339ad54324305c
Laminariy <dovakin121212@gmail.com>
2022-07-10 20:57:53 +0300

    new vkscript generator

--- vk-luvit/utils/queue_to_vkscript.lua
+++ vk-luvit/utils/queue_to_vkscript.lua
@@ -1,43 +1,18 @@
+local stringify = require('json').stringify
+
 --- Parse queue table to VKScript
 -- @param queue (table) - queue table (25 or less elements)
 -- @return vkscript (string) - correct VKScript
 return function(queue)
   assert(#queue<=25, 'Queue must be 25 or less elements!')
 
-  local vk_script_str = "var res=[];{BODY}return res;"
-  local method_str = "res.push(API.{METHOD}({{PARAMS}}));"
-  local param_str = "{PARAM_NAME}:{PARAM_VAL}"
-  local body_tbl, params_tbl = {}, {}
-  local tmp_param_str, tmp_method_str
+  local vk_script_str = "var res=[];%sreturn res;"
+  local method_str = "res.push(API.%s(%s));"
+  local body_tbl = {}
   local method, params
   for _, req in ipairs(queue) do
     method, params = unpack(req)
-
-    -- params
-    for param_name, param_val in pairs(params or {}) do
-      tmp_param_str = string.gsub(param_str, "{PARAM_NAME}", string.format('"%s"', param_name))
-      if param_name == "keyboard" then
-        tmp_param_str = string.gsub(tmp_param_str, "{PARAM_VAL}", string.format('%q', param_val))
-      elseif type(param_val) == "string" then
-        param_val = string.format('%q', param_val):gsub('\\\n', '\\n')
-        tmp_param_str = string.gsub(tmp_param_str, "{PARAM_VAL}", param_val)
-      else
-        tmp_param_str = string.gsub(tmp_param_str, "{PARAM_VAL}", param_val)
-      end
-      table.insert(params_tbl, tmp_param_str)
-    end
-
-    -- method
-    tmp_method_str = string.gsub(method_str, "{METHOD}", method)
-    if next(params_tbl) then
-      tmp_method_str = string.gsub(tmp_method_str, "{PARAMS}", table.concat(params_tbl, ","))
-    else
-      tmp_method_str = string.gsub(tmp_method_str, "{{PARAMS}}", '')
-    end
-    table.insert(body_tbl, tmp_method_str)
-
-    params_tbl = {}
+    table.insert(body_tbl, method_str:format(method, params and stringify(params) or ''))
   end
-
-  return string.gsub(vk_script_str, "{BODY}", table.concat(body_tbl))
+  return vk_script_str:format(table.concat(body_tbl))
 end
